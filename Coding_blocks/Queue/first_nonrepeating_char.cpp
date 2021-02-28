d52f53d8a35e65f5e47d7ca39d744be908b8276c
Pranjal Srivastava <pranjals149@gmail.com>
2021-02-28 20:58:46 +0530

    First NonRepeating Char Coding Blocks

---
+++ Coding_blocks/Queue/first_nonrepeating_char.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    queue<char> q;
+    int freq[24] = {0};
+
+    char ch;
+    cin >> ch;
+    while (ch != '.')
+    {
+        q.push(ch);
+        freq[ch - 'a']++;
+
+        while (!q.empty())
+        {
+            int idx = q.front() - 'a';
+
+            if (freq[idx] > 1)
+            {
+                q.pop();
+            }
+            else
+            {
+                cout << q.front() << endl;
+                break;
+            }
+        }
+
+        if (q.empty())
+        {
+            cout << "-1" << endl;
+        }
+
+        cin >> ch;
+    }
+}
