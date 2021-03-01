04485672fe45d922e9222f6ec563273588680655
Pranjal Srivastava <pranjals149@gmail.com>
2021-03-01 13:55:22 +0530

    Coding Blocks level order

---
+++ Coding_blocks/Binary_Tree/level_order_recursion.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int d)
+    {
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+node *buildTree()
+{
+    int d;
+    cin >> d;
+    if (d == -1)
+    {
+        return NULL;
+    }
+
+    node *root = new node(d);
+    root->left = buildTree();
+    root->right = buildTree();
+    return root;
+}
+
+void print(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    cout << root->data << " ";
+    print(root->left);
+    print(root->right);
+}
+
+int height(node *root)
+{
+    if (root == NULL)
+        return 0;
+
+    int ls = height(root->left);
+    int rs = height(root->right);
+    return max(ls, rs) + 1;
+}
+
+void printKthLevel(node *root, int k)
+{
+    if (root == NULL)
+        return;
+
+    if (k == 1)
+    {
+        cout << root->data << " ";
+        return;
+    }
+
+    printKthLevel(root->left, k - 1);
+    printKthLevel(root->right, k - 1);
+    return;
+}
+
+void printAllLevels(node *root)
+{
+    int H = height(root);
+
+    for (int i = 1; i < H; i++)
+    {
+        printKthLevel(root, i);
+        cout << endl;
+    }
+    return;
+}
+
+int main()
+{
+    node *root = buildTree();
+
+    cout << "height of the tree " << height(root) << endl;
+    printAllLevels(root);
+    return 0;
+}
