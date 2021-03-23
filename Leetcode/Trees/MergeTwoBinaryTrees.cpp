4e4ae2508baa9c20f15af91a85508e36efdce7b6
Pranjal Srivastava <pranjals149@gmail.com>
2021-03-23 21:08:26 +0530

    Trees Leetcode

---
+++ Leetcode/Trees/MergeTwoBinaryTrees.cpp
@@ -0,0 +1,29 @@
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *mergeTrees(TreeNode *root1, TreeNode *root2)
+    {
+        if (root1 && root2)
+        {
+
+            TreeNode *newNode = new TreeNode(root1->val + root2->val);
+            newNode->left = mergeTrees(root1->left, root2->left);
+            newNode->right = mergeTrees(root1->right, root2->right);
+            return newNode;
+        }
+        else
+        {
+            return root1 ? root1 : root2;
+        }
+    }
+};
