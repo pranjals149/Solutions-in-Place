7c2b9ecf2a8ab398a5960e7e2d83790ffae07c40
Pranjal Srivastava <pranjals149@gmail.com>
2021-03-26 23:13:54 +0530

    Coding_blocks Hashtable unordered_map

---
+++ Coding_blocks/Hashtable/unordered_map.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    unordered_map<string, int> um;
+    um.insert(make_pair("Mango", 10));
+
+    //same as map_stl
+}
