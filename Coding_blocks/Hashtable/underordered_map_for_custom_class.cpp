4bde451dbea4441fbd8e07f28584eefef0bb1088
Pranjal Srivastava <pranjals149@gmail.com>
2021-03-27 11:32:44 +0530

    Coding_blocks Hashtable underordered_map_for_custom_class

--- Coding_blocks/Hashtable/underordered_map_for_custom_class.cpp
+++ Coding_blocks/Hashtable/underordered_map_for_custom_class.cpp
@@ -4,23 +4,53 @@
 
 using namespace std;
 
-class Student {
-    public:
-        string firstname;
-        string lastname;
-        string rollno;
+class Student
+{
+public:
+    string firstname;
+    string lastname;
+    string rollno;
 
-        Student(string f, string l, string no) {
-            firstname = f;
-            lastname = l;
-            rollno = no;
-        }
+    Student(string f, string l, string no)
+    {
+        firstname = f;
+        lastname = l;
+        rollno = no;
+    }
+
+    bool operator==(const Student &s) const
+    {
+        return rollno == s.rollno;
+    }
 };
 
-int main() {
+class HashFn
+{
+public:
+    size_t operator()(const Student &s) const
+    {
+        return s.firstname.length() + s.lastname.length();
+    }
+};
+
+int main()
+{
+
+    unordered_map<Student, int, HashFn> student_map;
     Student s1("Pranjal", "Sri", "10");
-    Student s2("Pranjal", "Sri", "10");
-    Student s3("Pranjal", "Sri", "10");
-    Student s4("Pranjal", "Sri", "10");
+    Student s2("Prateek", "Sri", "11");
+    Student s3("Rahul", "Sri", "12");
+    Student s4("Ramesh", "Sri", "13");
+
+    //add students to hash map
+    student_map[s1] = 100;
+    student_map[s2] = 110;
+    student_map[s3] = 120;
+    student_map[s4] = 130;
+
+    for (auto s : student_map)
+    {
+        cout << s.first.firstname << " " << s.first.rollno << "Marks: " << s.second << endl;
+    }
     return 0;
 }
