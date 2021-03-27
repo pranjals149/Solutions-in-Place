dc3e96163674ab71412c6f1b726b531c7977ff12
Pranjal Srivastava <pranjals149@gmail.com>
2021-03-27 21:46:57 +0530

    Coding_blocks Hashtable vertical_order_print

---
+++ Coding_blocks/Hashtable/vertical_order_print.cpp
@@ -0,0 +1,38 @@
+// Given a tree .. print the elements of a tree in a vertical order.
+
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int d)
+    {
+        data = d;
+        left = right = NULL;
+    }
+};
+
+int main()
+{
+
+    node *root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+
+    root->right->left = new node(6);
+    root->right->right = new node(7);
+
+    root->right->right->right = new node(9);
+    root->left->right->right = new node(8);
+
+}
