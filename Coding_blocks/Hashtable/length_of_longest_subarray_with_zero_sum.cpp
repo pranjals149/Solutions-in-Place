0bbd138e4da017a60f65ed09ff5946aa0f999fda
Pranjal Srivastava <pranjals149@gmail.com>
2021-04-05 17:06:58 +0530

    Coding_blocks Hashtable length_of_longest_subarray_with_zero_sum

--- Coding_blocks/Hashtable/length_of_longest_subarray_with_zero_sum.cpp
+++ Coding_blocks/Hashtable/length_of_longest_subarray_with_zero_sum.cpp
@@ -1,6 +1,51 @@
 //Find the length of longest subarray with zero sum
-
 #include <iostream>
-#include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
+
+int longestSubarray0Sum(int arr[], int n)
+{
+    unordered_map<int, int> m;
+    int pre = 0;
+
+    int len = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        pre += arr[i];
+
+        if (pre == 0)
+        {
+            len = max(len, i + 1);
+        }
+
+        if (m.find(pre) != m.end())
+        {
+            len = max(len, i - m[pre]);
+        }
+        else
+        {
+            m[pre] = i;
+        }
+    }
+
+    return len;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << longestSubarray0Sum(arr, n) << endl;
+
+    return 0;
+}
