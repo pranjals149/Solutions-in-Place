04a3c6a573275b249bc254e534e9b0e1d4b3ffbd
Pranjal Srivastava <pranjals149@gmail.com>
2021-06-17 14:29:10 +0530

    Leetcode DP

---
+++ Leetcode/MinCostClimbingStairs.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    int minCostClimbingStairs(vector<int> &cost)
+    {
+        int n = cost.size();
+
+        vector<int> dp(n + 1, 0);
+
+        for (int i = 2; i <= n; i++)
+        {
+            dp[i] = min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1]);
+        }
+
+        return dp[n];
+    }
+};
