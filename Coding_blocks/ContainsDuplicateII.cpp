06bddea99f6d771fd7a8faac827c0da51569b376
Pranjal Srivastava <pranjals149@gmail.com>
2021-06-24 14:19:32 +0530

    Contains duplicate

---
+++ Coding_blocks/ContainsDuplicateII.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    bool containsNearbyDuplicate(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> m;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (m.count(nums[i]) > 0 and abs(i - m[nums[i]]) <= k)
+            {
+                return true;
+            }
+
+            m[nums[i]] = i;
+        }
+
+        return false;
+    }
+};
