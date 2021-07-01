1100ec666055807b15fd6adbf562e62aa20e64a3
Pranjal Srivastava <pranjals149@gmail.com>
2021-07-01 14:50:29 +0530

    Climbing Stairs

---
+++ self/DP(1D)/ClimbingStairs.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int climbStairs(int n)
+    {
+        if (n <= 2)
+            return n;
+
+        vector<int> dp(n);
+
+        dp[0] = 1;
+        dp[1] = 2;
+
+        for (int i = 2; i < n; i++)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n - 1];
+    }
+};
