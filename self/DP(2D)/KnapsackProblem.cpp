71f115f7ac0475ae800b371cd8fef8bfda53169a
Pranjal Srivastava <pranjals149@gmail.com>
2021-07-06 17:15:01 +0530

    Knapsack Problem - Bottom UP Appraoch

--- self/DP(2D)/KnapsackProblem.cpp
+++ self/DP(2D)/KnapsackProblem.cpp
@@ -34,6 +34,31 @@ int knapsack(int wts[], int prices[], int N, int W)
     return max(inc, exc);
 }
 
+//Bottom Up DP
+int knapsackDP(int wt[], int price[], int N, int W)
+{
+    vector<vector<int>> dp(N + 1, vector<int>(W + 1, 0));
+
+    for (int n = 1; n <= N; n++)
+    {
+        for (int w = 1; w <= W; w++)
+        {
+            int inc = 0, exc = 0;
+
+            if (wt[n - 1] <= w)
+            {
+                inc = price[n - 1] + dp[n - 1][w - wt[n - 1]];
+            }
+
+            exc = dp[n - 1][w];
+
+            dp[n][w] = max(inc, exc);
+        }
+    }
+
+    return dp[N][W];
+}
+
 int main()
 {
     int wts[] = {2, 7, 3, 4};
@@ -43,4 +68,7 @@ int main()
     int W = 11;
 
     cout << knapsack(wts, prices, N, W) << endl;
+    cout << knapsackDP(wts, prices, N, W) << endl;
+
+    return 0;
 }
