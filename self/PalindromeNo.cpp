18d6f5b2ea47acf62d30e931055b1c169afc9ef7
Pranjal Srivastava <pranjals149@gmail.com>
2021-07-11 13:57:01 +0530

    Palindrome No

---
+++ self/PalindromeNo.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool isPalindrome(int x)
+    {
+        string s = to_string(x);
+
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i <= j)
+        {
+            if (s[i] == s[j])
+            {
+                i++;
+                j--;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
