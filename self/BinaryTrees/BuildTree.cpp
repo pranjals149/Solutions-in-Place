ccdcb3e1cad083a3b3f389436d6c23fad3a521eb
Pranjal Srivastava <pranjals149@gmail.com>
2021-07-15 11:54:49 +0530

    Build Tree BoilerPlate

---
+++ self/BinaryTrees/BuildTree.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int d)
+    {
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+// PreOrder - Root, Left, Right
+node *buildTree()
+{
+    int d;
+    cin >> d;
+
+    if (d == -1)
+    {
+        return NULL;
+    }
+
+    node *n = new node(d);
+    n->left = buildTree();
+    n->right = buildTree();
+
+    return n;
+}
+
+int main()
+{
+    node *root = buildTree();
+}
