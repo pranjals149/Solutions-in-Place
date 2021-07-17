04e097c5b667caa7e6ab573d72f5d23138645e46
Pranjal Srivastava <pranjals149@gmail.com>
2021-07-17 14:04:50 +0530

    Range Sum BST

--- self/BinaryTrees/ RangeSumofBST.cpp
+++ self/BinaryTrees/ RangeSumofBST.cpp
@@ -1,41 +1,39 @@
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode left, TreeNode right) : val(x), left(left), right(right) {}
+};
 
-  Definition for a binary tree node.
-  struct TreeNode
-  {
-      int val;
-      TreeNode *left;
-      TreeNode *right;
-      TreeNode() : val(0), left(nullptr), right(nullptr) {}
-      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-      TreeNode(int x, TreeNode left, TreeNode right) : val(x), left(left), right(right) {}
-  };
+class Solution
+{
+public:
+    int sum = 0;
 
-  class Solution
-  {
-  public:
-      int sum = 0;
+    int solve(TreeNode *root, int low, int high)
+    {
+        if (root)
+        {
+            solve(root->left, low, high);
 
-      int solve(TreeNode *root, int low, int high)
-      {
-          if (root)
-          {
-              solve(root->left, low, high);
+            if (root->val >= low and root->val <= high)
+            {
+                sum += root->val;
+            }
 
-              if (root->val >= low and root->val <= high)
-              {
-                  sum += root->val;
-              }
+            solve(root->right, low, high);
+        }
 
-              solve(root->right, low, high);
-          }
+        return sum;
+    }
 
-          return sum;
-      }
-
-      int rangeSumBST(TreeNode *root, int low, int high)
-      {
-          if (!root)
-              return 0;
-          return solve(root, low, high);
-      }
-  };
+    int rangeSumBST(TreeNode *root, int low, int high)
+    {
+        if (!root)
+            return 0;
+        return solve(root, low, high);
+    }
+};
