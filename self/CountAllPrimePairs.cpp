872623ba1962e8afb18de934d594b911df63b479
Pranjal Srivastava <pranjals149@gmail.com>
2021-07-20 16:50:02 +0530

    Count all prime pairs

---
+++ self/CountAllPrimePairs.cpp
@@ -0,0 +1,27 @@
+// There is a range given n and m in which we have to find the count of all the prime pairs whose difference is 6. We have to find how many sets are there within a given range.
+
+//     Output :
+
+//     The output consists of a single line,
+//     print the count prime pairs in a given range.Else print "No Prime Pairs".
+
+//         Constraints :
+
+//         2 <= n <= 1000
+
+//         n <= m <= 2000
+
+//                   Sample Input :
+
+//                   4
+
+//                   30
+
+//                   Output :
+
+//                   6
+
+//                   Explanation :
+
+//     (5, 11)(7, 13)(11, 17)(13, 19)(17, 23)(23, 29)
+//         .we have 6 prime pairs.
