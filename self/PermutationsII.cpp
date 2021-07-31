e95e7112c71a960df8fa783640f9510037fd35c8
Pranjal Srivastava <pranjals149@gmail.com>
2021-07-31 22:21:41 +0530

    Next permutations cpp

---
+++ self/PermutationsII.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<vector<int>> permuteUnique(vector<int> &nums)
+    {
+        vector<vector<int>> ans;
+
+        sort(nums.begin(), nums.end());
+        helper(nums, ans, 0);
+
+        return ans;
+    }
+
+private:
+    void helper(vector<int> nums, vector<vector<int>> &ans, int idx)
+    {
+        if (idx == nums.size())
+        {
+            ans.push_back(nums);
+        }
+
+        for (int i = idx; i < nums.size(); i++)
+        {
+            if (i != idx and nums[i] == nums[idx])
+            {
+                continue;
+            }
+
+            swap(nums[i], nums[idx]);
+            helper(nums, ans, idx + 1);
+        }
+    }
+};
