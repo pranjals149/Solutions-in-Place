c097292c75dffb58a62e0aea2905bf40978aa18a
Pranjal Srivastava <pranjals149@gmail.com>
2021-08-01 15:32:56 +0530

    Merge Sorted Array

---
+++ self/MergeSortedArray.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+public:
+    void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
+    {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (i >= 0 and j >= 0)
+        {
+            if (nums1[i] > nums2[j])
+            {
+                nums1[k] = nums1[i];
+                k--;
+                i--;
+            }
+            else
+            {
+                nums1[k] = nums2[j];
+                k--;
+                j--;
+            }
+        }
+
+        while (j >= 0)
+        {
+            nums1[k] = nums2[j];
+            k--;
+            j--;
+        }
+    }
+};
