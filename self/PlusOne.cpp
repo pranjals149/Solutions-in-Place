aa0454fd05bee1b7c3336d252f94119d55e7ceb1
Pranjal Srivastava <pranjals149@gmail.com>
2021-08-01 14:45:16 +0530

    Plus One

---
+++ self/PlusOne.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> plusOne(vector<int> &digits)
+    {
+        for (int i = digits.size() - 1; i >= 0; i--)
+        {
+            if (digits[i] != 9)
+            {
+                digits[i]++;
+                break;
+            }
+            else
+            {
+                digits[i] = 0;
+            }
+        }
+
+        if (digits[0] == 0)
+        {
+            digits.insert(digits.begin(), 1);
+        }
+
+        return digits;
+    }
+};
