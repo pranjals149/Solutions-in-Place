bef4d05191cc3c8ece7c373d34432061ca85df38
Pranjal Srivastava <pranjals149@gmail.com>
2021-08-04 12:44:58 +0530

    Single Number

---
+++ self/SingleNumber.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+        unordered_map<int, int> m;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            m[nums[i]]++;
+        }
+
+        for (auto i : m)
+        {
+            if (i.second == 1)
+            {
+                return i.first;
+            }
+        }
+
+        return 0;
+    }
+};
