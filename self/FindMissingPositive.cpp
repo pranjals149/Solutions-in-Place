8e031cba5aae65707ce7c820aa7fb95b7211002f
Pranjal Srivastava <pranjals149@gmail.com>
2021-08-24 23:09:21 +0530

    Find Missing Positive

---
+++ self/FindMissingPositive.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Solution
+{
+public:
+    int firstMissingPositive(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+
+        int min_ele = 1;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == min_ele)
+            {
+                min_ele++;
+            }
+        }
+
+        return min_ele;
+    }
+};
