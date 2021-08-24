230d8214defda3357a9139a476a072674feafa07
Pranjal Srivastava <pranjals149@gmail.com>
2021-08-24 22:59:07 +0530

    Combination Sum 2

---
+++ self/CombinationSum2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    void helper(vector<int> &candidates, vector<int> &curr, vector<vector<int>> &ans, int target, int curr_idx)
+    {
+        if (target == 0)
+        {
+            ans.push_back(curr);
+            return;
+        }
+
+        for (int i = curr_idx; i < candidates.size(); i++)
+        {
+            if (i > curr_idx and candidates[i] == candidates[i - 1])
+            {
+                continue;
+            }
+
+            if (candidates[i] <= target)
+            {
+                curr.push_back(candidates[i]);
+                helper(candidates, curr, ans, target - candidates[i], i + 1);
+                curr.pop_back();
+            }
+        }
+    }
+
+    vector<vector<int>> combinationSum2(vector<int> &candidates, int target)
+    {
+        sort(candidates.begin(), candidates.end());
+
+        vector<int> curr;
+        vector<vector<int>> ans;
+
+        helper(candidates, curr, ans, target, 0);
+
+        return ans;
+    }
+};
