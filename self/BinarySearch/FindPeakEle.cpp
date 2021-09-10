69eab2ee205c1bd8e910ce104114d712712fbf9d
Pranjal Srivastava <pranjals149@gmail.com>
2021-09-10 21:09:39 +0530

    Find in Mountain Array

--- self/BinarySearch/FindPeakEle.cpp
+++ self/BinarySearch/FindPeakEle.cpp
@@ -29,3 +29,43 @@ public:
         return s;
     }
 };
+
+// int orderAgnosticBS(MountainArray &mountainArr, int target, int start, int end)
+// {
+//     bool isAsc = mountainArr.get(start) < mountainArr.get(end);
+
+//     while (start <= end)
+//     {
+//         int mid = start + (end - start) / 2;
+
+//         if (mountainArr.get(mid) == target)
+//         {
+//             return mid;
+//         }
+
+//         if (isAsc)
+//         {
+//             if (target < mountainArr.get(mid))
+//             {
+//                 end = mid - 1;
+//             }
+//             else
+//             {
+//                 start = mid + 1;
+//             }
+//         }
+//         else
+//         {
+//             if (target > mountainArr.get(mid))
+//             {
+//                 end = mid - 1;
+//             }
+//             else
+//             {
+//                 start = mid + 1;
+//             }
+//         }
+//     }
+
+//     return -1;
+// }
