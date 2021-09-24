2d0a771670d629422b3a85ffb1ae7c5f7ecbb220
Pranjal Srivastava <pranjals149@gmail.com>
2021-09-24 14:07:01 +0530

    Nth Tribonacci no

---
+++ self/NthTribonacciNo.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        
+        vector<int> dp(n+1, 0);
+        
+        dp[1] = 1;
+        dp[2] = 1;
+        
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i-3] + dp[i-2] + dp[i-1];
+        }
+        
+        return dp[n];
+    }
+};
